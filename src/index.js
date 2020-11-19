e3a9bbe7ed7a6a1a568d560ef5681842fe4d3f82
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-11-19 22:25:27 +0900

    11/19 add update

--- src/index.js
+++ src/index.js
@@ -46,18 +46,34 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-//アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+const myProfile = {
+  name: "上本",
+  age: 32
 };
-console.log(func3(10, 20));
+
+const message1 = `名前は${myProfile.name}です`;
+console.log(message1);
+
+const { name, age } = myProfile;
+
+const message2 = `名前は${name}です`;
+console.log(message2);
