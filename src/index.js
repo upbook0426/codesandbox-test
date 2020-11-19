819864407b00c0a2129655a020ca05c7e20ed31e
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-11-19 22:14:52 +0900

    11/19　update

--- src/index.js
+++ src/index.js
@@ -16,9 +16,48 @@
 // //letは再宣言不可能
 // let val2 ="再宣言";
 
-const val4 = {
-  name: "seiji",
-  age: 32
+// const val4 = {
+//   name: "seiji",
+//   age: 32
+// };
+// val4.name = "jak";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5.push("monkey");
+// console.log(val5);
+
+//**テンプレート文字列
+
+// const name = "上本";
+// const age = 28;
+
+//従来の方法
+// const message1 = "私の名前は" +　name + "です";
+// console.log(message1);
+
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
 };
-val4.name = "jak";
-console.log(val4);
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
