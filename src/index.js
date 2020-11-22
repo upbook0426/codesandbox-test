b8779ebdd917608f4559c1be2fb8bf3effba4e7d
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-11-22 23:09:43 +0900

    11/22 update

--- src/index.js
+++ src/index.js
@@ -65,15 +65,30 @@
 /**
  * 分割代入
  */
-const myProfile = {
-  name: "上本",
-  age: 32
-};
+// const myProfile = {
+//   name: "上本",
+//   age: 32
+// };
 
-const message1 = `名前は${myProfile.name}です`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}です`;
+// console.log(message1);
 
-const { name, age } = myProfile;
+// const { name, age } = myProfile;
 
-const message2 = `名前は${name}です`;
-console.log(message2);
+// const message2 = `名前は${name}です`;
+// console.log(message2);
+
+// const myProfile = ['せいじ',32];
+
+// const message3 = `名前は${myProfile[0]},年齢は${myProfile[1]}`;
+// console.log(message3);
+
+// const [name,age] = myProfile;
+// const message4 = `名前は${name}です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+sayHello("せいじ");
