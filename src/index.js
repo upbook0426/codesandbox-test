4cc192cfd004ab076ae8724612abfaf9de1b3866
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-11-23 23:49:25 +0900

    11/23 map filter

--- src/index.js
+++ src/index.js
@@ -78,51 +78,32 @@
 // const message2 = `名前は${name}です`;
 // console.log(message2);
 
-// const myProfile = ['せいじ',32];
-
-// const message3 = `名前は${myProfile[0]},年齢は${myProfile[1]}`;
-// console.log(message3);
-
-// const [name,age] = myProfile;
-// const message4 = `名前は${name}です。`;
-// console.log(message4);
-
 /**
- * デフォルト値、引数など
+ * mapやfillterを使った配列の処理
 //  */
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-// sayHello("せいじ");
+const nameArr = ["田中", "山田", "上本"];
+// for(let index = 0 ; index < nameArr.length; index++){
+//   console.log(`${index}番目は、${nameArr[index]}です`);
+// }
 
-/**
- * スプレッド構文...
- */
-//配列の展開
-// const arr1 = [1, 2];
-// console.log(arr1);
-// console.log(...arr1);
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
 
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]);
-// sumFunc(...arr1);
+// nameArr.map((name) => console.log(name));
 
-//まとめる
-// const arr2 = [1,2,3,4,5];
-// const [num1,num2,...arr3] = arr2;
-// console.log(num1);
-// console.log(num2);
-// console.log(arr3);
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
 
-//配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
-
-// const arr6 =[...arr4];
-// console.log(arr6);
-
-// const arr7 =[...arr4,...arr5];
-// console.log(arr7);
-
-const arr8 = arr4;
-console.log(arr8);
-arr8[0] = 100;
-console.log(arr4);
+const newNameArr = nameArr.map((name) => {
+  if (name === "上本") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
