d8a26d8922cd9440a73045b51eb21a5fe54f842d
Upbook <58377749+upbook0426@users.noreply.github.com>
2020-11-25 21:32:11 +0900

    11/25 　update

--- src/index.js
+++ src/index.js
@@ -65,20 +65,19 @@
 /**
  * 分割代入
  */
-// const myProfile = {
-//   name: "上本",
-//   age: 32
-// };
+const myProfile = {
+  name: "上本",
+  age: 32
+};
 
-// const message1 = `名前は${myProfile.name}です`;
-// console.log(message1);
+const message1 = `名前は${myProfile.name}です`;
+console.log(message1);
 
-// const { name, age } = myProfile;
+const { name, age } = myProfile;
 
 // const message2 = `名前は${name}です`;
 // console.log(message2);
 
-/**
  * mapやfillterを使った配列の処理
 //  */
 const nameArr = ["田中", "山田", "上本"];
